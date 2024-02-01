321a00527a582648eef2b6136412107e186172e6
Celeste Loi <loiceleste94@gmail.com>
2024-02-01 17:08:52 +0000

    adjust coffee cards to display required coffee info

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,12 @@
 export default function Card({ coffee }) {
-  return <div>{coffee.name}</div>;
+  return (
+    <div>
+      <img src={coffee.image} alt="" />
+      <h2>{coffee.name}</h2>
+      <p>{coffee.price}</p>
+      <p>
+        ⭐{coffee.rating} ({coffee.votes} votes)
+      </p>
+    </div>
+  );
 }
