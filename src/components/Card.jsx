f6238696b42824331a33d8bf4212fa5a3cede08e
Celeste Loi <loiceleste94@gmail.com>
2024-02-01 17:16:22 +0000

    render conditionally cooffee popularity and availability

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,13 @@ export default function Card({ coffee }) {
   return (
     <div>
       <img src={coffee.image} alt="" />
+      {coffee.popular == true ? <p>popular</p> : ""}
       <h2>{coffee.name}</h2>
       <p>{coffee.price}</p>
       <p>
         ⭐{coffee.rating} ({coffee.votes} votes)
       </p>
+      {coffee.available == false ? <p>Sold out</p> : ""}
     </div>
   );
 }
